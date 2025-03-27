8b4ac5c5a6959b7da9d4b2a4c6ad7f531e40eae8
D'Anjelo Gonzalez <d.gonzalez1@students.mq.edu.au>
2025-03-27 14:59:28 +1100

    secondfinalwk5

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -37,13 +37,11 @@ public class App {
 
         wordleDatabaseConnection.createNewDatabase("words.db");
         if (wordleDatabaseConnection.checkIfConnectionDefined()) {
-            System.out.println("Wordle created and connected.");
         } else {
             System.out.println("Not able to connect. Sorry!");
             return;
         }
         if (wordleDatabaseConnection.createWordleTables()) {
-            System.out.println("Wordle structures in place.");
         } else {
             System.out.println("Not able to launch. Sorry!");
             return;
